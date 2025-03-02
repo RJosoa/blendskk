12f196cec1f0897d77751aa7c6daecbb9a609f75
xSKUUUx <r.josoaf@gmail.com>
2025-03-02 13:06:06 +0100

    Refactor post templates and API; consolidate forms and implement new PostsApiController for enhanced post management

---
+++ src/Controller/Frontend/PostsController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Controller\Frontend;
+
+use App\Entity\Posts;
+use App\Form\PostsType;
+use App\Repository\PostsRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+#[Route('/posts')]
+final class PostsController extends AbstractController{
+    #[Route(name: 'app_posts_index', methods: ['GET'])]
+    public function index(PostsRepository $postsRepository): Response
+    {
+        return $this->render('posts/index.html.twig', [
+            'posts' => $postsRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_posts_new', methods: ['GET', 'POST'])]
+    #[IsGranted('ROLE_USER')]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $post = new Posts();
+        $post->setUser($this->getUser());
+        $form = $this->createForm(PostsType::class, $post);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($post);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_posts_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('posts/new.html.twig', [
+            'post' => $post,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_posts_show', methods: ['GET'])]
+    #[IsGranted('ROLE_USER')]
+    public function show(Posts $post): Response
+    {
+        return $this->render('posts/show.html.twig', [
+            'post' => $post,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_posts_edit', methods: ['GET', 'POST'])]
+    #[IsGranted('ROLE_USER')]
+    public function edit(Request $request, Posts $post, EntityManagerInterface $entityManager): Response
+    {
+        if ($post->getUser() !== $this->getUser()) {
+            throw $this->createAccessDeniedException('You can only edit your own posts.');
+        }
+        $form = $this->createForm(PostsType::class, $post);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_posts_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('posts/edit.html.twig', [
+            'post' => $post,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_posts_delete', methods: ['POST'])]
+    #[IsGranted('ROLE_USER')]
+    public function delete(Request $request, Posts $post, EntityManagerInterface $entityManager): Response
+    {
+        if ($post->getUser() !== $this->getUser()) {
+            throw $this->createAccessDeniedException('You can only delete your own post.');
+        }
+        if ($this->isCsrfTokenValid('delete'.$post->getId(), $request->getPayload()->getString('_token'))) {
+            $entityManager->remove($post);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_posts_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
