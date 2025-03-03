bd933710544d413e4113c1e51df48c22384f9fef
xSKUUUx <r.josoaf@gmail.com>
2025-03-03 14:17:15 +0100

    Implement user profile management with edit and delete functionality; add TipsController for tips page

--- src/Controller/Frontend/ExplorerController.php
+++ src/Controller/Frontend/ExplorerController.php
@@ -2,16 +2,18 @@
 
 namespace  App\Controller\Frontend;
 
+use App\Repository\PostsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class ExplorerController extends AbstractController{
     #[Route('/explorer', name: 'app_explorer')]
-    public function index(): Response
+    public function index(PostsRepository $postsRepository): Response
     {
         return $this->render('explorer/index.html.twig', [
             'controller_name' => 'Frontend/ExplorerController',
+            'posts' => $postsRepository->findAll(),
         ]);
     }
 }
