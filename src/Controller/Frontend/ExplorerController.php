2fa28d8fa40906ad1c22b8a7247af0fdc6e6bf54
xSKUUUx <r.josoaf@gmail.com>
2025-03-04 19:23:28 +0100

    Add favorites functionality to explorer; update controller and template for favorites integration

--- src/Controller/Frontend/ExplorerController.php
+++ src/Controller/Frontend/ExplorerController.php
@@ -2,6 +2,7 @@
 
 namespace  App\Controller\Frontend;
 
+use App\Repository\FavoritesRepository;
 use App\Repository\LikesRepository;
 use App\Repository\PostsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -10,13 +11,14 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class ExplorerController extends AbstractController{
     #[Route('/explorer', name: 'app_explorer')]
-    public function index(PostsRepository $postsRepository, LikesRepository $likesRepository): Response
+    public function index(PostsRepository $postsRepository, LikesRepository $likesRepository, FavoritesRepository $favoritesRepository): Response
     {
         $posts = $postsRepository->findAll();
         return $this->render('explorer/index.html.twig', [
             'controller_name' => 'Frontend/ExplorerController',
             'posts' => $posts,
-            'likesRepository' => $likesRepository
+            'likesRepository' => $likesRepository,
+            'favoritesRepository' => $favoritesRepository
         ]);
     }
 }
