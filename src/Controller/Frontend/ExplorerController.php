8985d757aa66efff2c59a7eb10b9691f2e2116ac
xSKUUUx <r.josoaf@gmail.com>
2025-03-06 13:53:37 +0100

    Implement pagination in ExplorerController; update index method to retrieve paginated posts and add pagination component in the template

--- src/Controller/Frontend/ExplorerController.php
+++ src/Controller/Frontend/ExplorerController.php
@@ -6,19 +6,35 @@ use App\Repository\FavoritesRepository;
 use App\Repository\LikesRepository;
 use App\Repository\PostsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class ExplorerController extends AbstractController{
     #[Route('/explorer', name: 'app_explorer')]
-    public function index(PostsRepository $postsRepository, LikesRepository $likesRepository, FavoritesRepository $favoritesRepository): Response
-    {
-        $posts = $postsRepository->findAll();
+    public function index(
+        PostsRepository $postsRepository,
+        LikesRepository $likesRepository,
+        FavoritesRepository $favoritesRepository,
+        Request $request
+    ): Response {
+        $page = $request->query->getInt('page', 1);
+
+        $paginatedPosts = $postsRepository->findPaginated($page, 12);
+
         return $this->render('explorer/index.html.twig', [
             'controller_name' => 'Frontend/ExplorerController',
-            'posts' => $posts,
+            'posts' => $paginatedPosts['data'],
+            'pagination' => [
+                'currentPage' => $paginatedPosts['currentPage'],
+                'totalPages' => $paginatedPosts['pageCount'],
+                'totalItems' => $paginatedPosts['totalItems'],
+                'limit' => $paginatedPosts['limit']
+            ],
             'likesRepository' => $likesRepository,
             'favoritesRepository' => $favoritesRepository
         ]);
     }
+
 }
