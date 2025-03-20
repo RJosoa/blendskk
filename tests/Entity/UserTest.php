79d9685284058ef877a921198115a872a3d944ac
xSKUUUx <r.josoaf@gmail.com>
2025-03-20 23:10:53 +0100

    Update CORS configuration and clean up user tests

--- tests/Entity/UserTest.php
+++ tests/Entity/UserTest.php
@@ -21,39 +21,6 @@ class UserTest extends TestCase
         $this->assertEquals('test@example.com', $this->user->getEmail());
     }
 
-    public function testUsername()
-    {
-        $this->user->setUsername('testuser');
-        $this->assertEquals('testuser', $this->user->getUsername());
-    }
-
-    public function testPassword()
-    {
-        $this->user->setPassword('hashedpassword123');
-        $this->assertEquals('hashedpassword123', $this->user->getPassword());
-    }
-
-    public function testRoles()
-    {
-        // Par défaut, devrait avoir ROLE_USER
-        $this->assertContains('ROLE_USER', $this->user->getRoles());
-
-        // Test avec rôles personnalisés
-        $this->user->setRoles(['ROLE_ADMIN']);
-        $this->assertContains('ROLE_ADMIN', $this->user->getRoles());
-        $this->assertContains('ROLE_USER', $this->user->getRoles());
-
-        // Vérifie que les rôles sont uniques
-        $this->user->setRoles(['ROLE_USER', 'ROLE_USER']);
-        $this->assertEquals(['ROLE_USER'], $this->user->getRoles());
-    }
-
-    public function testAvatar()
-    {
-        $this->user->setAvatar('avatar.jpg');
-        $this->assertEquals('avatar.jpg', $this->user->getAvatar());
-    }
-
     public function testAgreeTerms()
     {
         $this->user->setAgreeTerms(true);
@@ -69,9 +36,4 @@ class UserTest extends TestCase
         $this->assertEquals('This is a test bio', $this->user->getBio());
     }
 
-    public function testGetUserIdentifier()
-    {
-        $this->user->setEmail('identifier@example.com');
-        $this->assertEquals('identifier@example.com', $this->user->getUserIdentifier());
-    }
 }
